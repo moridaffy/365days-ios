eaeaba50fc04f5af7e24f235980a6aab79443786
Maxim Skryabin <mxmskrbn@gmail.com>
2022-04-05 18:31:57 +0300

    Project created

---
+++ three-six-five-days/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  three-six-five-days
+//
+//  Created by Maxim Skryabin on 05.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ three-six-five-days/three_six_five_daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  three_six_five_daysApp.swift
+//  three-six-five-days
+//
+//  Created by Maxim Skryabin on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct three_six_five_daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
